c31bf64035aaa829d3a124740265938754716ea6
Will <wjeckert15@gmail.com>
2020-12-29 22:26:44 -0600

    fixed bug

--- server.js
+++ server.js
@@ -24,13 +24,6 @@ app.get('/movies', function HandleGetMovies(req, res ){
     let movieSearch = [...movies];
     const {genre, country, avg_vote} = req.query;
     if (genre){
-        if (genre.toLowerCase() !== 'animation' && genre.toLowerCase() !== 'drama' && genre.toLowerCase() !== 'romantic' && genre.toLowerCase() !== 'comedy'
-            && genre.toLowerCase() !== 'spy' && genre.toLowerCase() !== 'crime' && genre.toLowerCase() !== 'thriller' && genre.toLowerCase() !== 'adventure'
-            && genre.toLowerCase() !== 'documentary' && genre.toLowerCase() !== 'horror' && genre.toLowerCase() !== 'action' && genre.toLowerCase() !== 'western' && genre.toLowerCase() !== 'history'
-            && genre.toLowerCase() !== 'biography' && genre.toLowerCase() !== 'musical' && genre.toLowerCase() !== 'fantasy' && genre.toLowerCase() !== 'war' && genre.toLowerCase() !== 'grotesque')
-        {
-            return res.status(400).send('Genre must be Action, Adventure, Animation, Biography, Comedy, Crime, Documentary, Drama, Fantasy, Grotesque, History, Horror, Musical, Romantic, Spy, War or Western ')
-        }
         movieSearch = movieSearch.filter(movie => {
             return movie.genre.toLowerCase().includes(genre.toLowerCase())
         })
